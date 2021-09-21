2b5aba0ab3f9acc6b5f15125f760837f92a9d701
Sahdev <dxdestroyer007@gmail.com>
2021-09-21 09:48:09 +0530

    4. FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Faqsection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur
+            nemo accusamus quibusdam corrupti debitis dolorem!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Ex iusto et, beatae cum eaque aut nisi ipsum possimus error,
+            assumenda molestiae, veritatis quas.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Soluta sequi pariatur ullam voluptates tempore? Suscipit, nulla!
+            Explicabo soluta recusandae, officiis a qui sapiente?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products Do You Offer ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Facilis fugit iste rerum consectetur cumque vel officiis. Quis
+            asperiores illo perferendis deleniti corrupti tenetur.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqsection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from "react";
 
 //import page components
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
 import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
@@ -9,6 +10,7 @@ const AboutUs = () => {
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
