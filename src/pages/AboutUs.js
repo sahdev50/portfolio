dab59e707f1c69245c199952927a74ec606ae578
Sahdev <dxdestroyer007@gmail.com>
2021-09-21 09:26:38 +0530

    3. services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,16 @@
 import React from "react";
+
+//import page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
